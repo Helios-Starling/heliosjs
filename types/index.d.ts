72b75a770fa452c1a00db9f369dfcd0301c8d7e4
Killian Di Vincenzo <hello@killiandvcz.fr>
2025-05-11 17:42:53 +0200

    feat: update version to 0.4.3 and enhance exports with type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+export { Helios } from './core/helios.js';
+
+// Types
+
+/**
+ * @typedef {import('./core/starling.js').Starling} Starling
+ */
+
+/**
+ * @typedef {import('@helios-starling/utils').RequestContext} RequestContext
+ * * @typedef {import('@helios-starling/utils').ResponseContext} ResponseContext
+ */
